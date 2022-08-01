320e347979a7a5f30946e65278cafe3ae86a8a01
lizhihua160 <2477309387@qq.com>
2022-08-01 22:37:46 +0800

    8/1

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,10 +1,10 @@
--- vim.g.sonokai_style = 'atlantis'
--- vim.g.sonokai_enable_italic = 0
--- vim.g.sonokai_disable_italic_comment = 1
--- vim.cmd('colorscheme sonokai')
+vim.g.sonokai_style = 'atlantis'
+vim.g.sonokai_enable_italic = 0
+vim.g.sonokai_disable_italic_comment = 1
+vim.cmd('colorscheme sonokai')
 --
-vim.opt.background = 'light'      -- set the background dark
-vim.cmd([[colorscheme solarized]])
+-- vim.opt.background = 'light'      -- set the background dark
+-- vim.cmd([[colorscheme solarized]])
 -- vim.cmd('colorscheme molokai')
 -- vim.opt.background = "light" -- or "light" for light mode
 -- vim.cmd([[colorscheme gruvbox]])
